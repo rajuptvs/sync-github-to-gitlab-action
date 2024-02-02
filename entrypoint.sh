4aa66abf07840628f6682b0bce13e17da989d028
rajuptvs <raju.ptvs@gmail.com>
2024-02-02 12:27:26 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+# Setup SSH and Git configuration
 mkdir -p /root/.ssh
 echo "$INPUT_DESTINATION_SSH_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
-
+ssh-keyscan gitlab.com >> /root/.ssh/known_hosts
 git config --global --add safe.directory /github/workspace
+
+# Ensure the "sync" branch exists
+git fetch origin $INPUT_DESTINATION_BRANCH_NAME:$INPUT_DESTINATION_BRANCH_NAME || git checkout -b $INPUT_DESTINATION_BRANCH_NAME origin/$INPUT_DESTINATION_BRANCH_NAME || git checkout $INPUT_DESTINATION_BRANCH_NAME
+
+# Add the remote and push
 git remote add mirror $INPUT_DESTINATION_REPOSITORY
 git push mirror $INPUT_DESTINATION_BRANCH_NAME -f
